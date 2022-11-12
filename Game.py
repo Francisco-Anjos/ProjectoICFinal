9c620a3969015b245576ce1a8b2d358ebf02e383
nightwolf anjels <fcariaanjos@gmail.com>
2022-11-12 15:32:44 +0000

    added the forth Orc and complemented code to match

--- Game.py
+++ Game.py
@@ -1,16 +1,18 @@
 import random 
 
-#Player and enemy classes in order(health,Mana,Armor,Weapon,Initiative)
+#Player and enemy class stats in order(health,Mana,Armor,Weapon,Initiative)
 WarriorClass = [32,5,2,5,2]
 PriestClass = [20,25,0,2,6]
 OrcClass1 = [15,0,2,2,2]
 OrcClass2 = [15,0,2,2,2]
 OrcClass3 = [15,0,2,2,2]
+OrcClass4 = [15,0,2,2,2]
+ClassList = [PriestClass,WarriorClass,OrcClass1,OrcClass2,OrcClass3,OrcClass4]
 
 
 #Action phases per player class and enemy class 
 #Warriorclass turn 
-def WarriorTurn(HP,WP,MP,AP,Rd):
+def WarriorTurn(HP1,HP2,HP3,HP4,WP,MP,AP1,AP2,AP3,AP4,Rd):
     print("Choose your action")
     print("1:Magic")
     print("2:Attack")
@@ -24,33 +26,37 @@ def WarriorTurn(HP,WP,MP,AP,Rd):
             if Target == 1:
                 print("You casted Rushdown on the first Orc")
                 MP = MP - 5
-                HP = HP - ((WP + Rd)-AP)
+                HP1 = HP1 - ((WP + Rd)-AP1)
             elif Target == 2:
                 print("You casted Rushdown on the second Orc")
                 MP = MP - 5
-                HP = HP - ((WP + Rd)-AP)
+                HP2 = HP2 - ((WP + Rd)-AP2)
             elif Target == 3:
                 print("You casted Rushdown on the third Orc")
                 MP = MP - 5
-                HP = HP - ((WP + Rd)-AP)
+                HP3 = HP3 - ((WP + Rd)-AP3)
+            elif Target == 4:
+                print("You casted Rushdown on the forth Orc")
+                MP = MP - 5
+                HP4 = HP4 - ((WP + Rd)-AP4)
     elif Action ==2:
          Target = input("Choose target:")
          if Target == 1:
             print("You struck the first Orc")
-            HP = HP - (WP - AP)
-            return(HP,MP)
+            HP1 = HP1 - (WP - AP1)
          elif Target == 2:
             print("You struck the second Orc")
-            HP = HP - (WP - AP)
-            return(HP,MP)
+            HP2 = HP2 - (WP - AP2)
          elif Target == 3:
             print("You struck the third Orc")
-            HP = HP - (WP - AP)
-            return(HP,MP)
-
+            HP3 = HP3 - (WP - AP3)
+         elif Target == 4:
+            print("You struck the forth Orc")
+            HP4 = HP4 - (WP - AP4)
+            return(HP1,HP2,HP3,HP4,MP)
 
 #Priestclass Turn 
-def PriestTurn(HP,WP,MP,AP,Ex,He):
+def PriestTurn(HP1,HP2,HP3,HP4,HP5,HP6,WP,MP,AP1,AP2,AP3,AP4,Ex,He):
     print("Choose your action")
     print("1:Magic")
     print("2:Attack")
@@ -63,59 +69,82 @@ def PriestTurn(HP,WP,MP,AP,Ex,He):
         if Spell == 1:
             Target = input("Choose target:")
             if Target == 1:
-                print("You healed yourself")
+                print("You mended your wounds")
                 MP = MP - 3
-                HP = HP + (He + WP )
+                HP5 = HP5 + (He + WP)
             elif Target == 2:
-                print("You healed yourself")
+                print("You mended your wounds")
                 MP = MP - 3
-                HP = HP + (He + WP )
+                HP6 = HP6 + (He + WP)
         elif Spell == 2:
             Target = input("Choose target:")
             if Target == 1:
-                print("You casted Exorcism")
-                HP = HP - (Ex - AP)
+                print("You casted Exorcism on the first Orc")
+                HP1 = HP1 - (Ex - AP1)
                 MP = MP - 5
             elif Target == 2:
-                print("You casted Exorcism")
-                HP = HP - (Ex - AP)
+                print("You casted Exorcism on the second Orc")
+                HP2 = HP2 - (Ex - AP2)
                 MP = MP - 5
-            elif Target == 2:
-                print("You casted Exorcism")
-                HP = HP - (Ex - AP)
+            elif Target == 3:
+                print("You casted Exorcism on the third Orc")
+                HP3 = HP3 - (Ex - AP3)
+                MP = MP - 5
+            elif Target == 4:
+                print("You casted Exorcism on the forth Orc")
+                HP4 = HP4 - (Ex - AP4)
                 MP = MP - 5
     elif Action ==2:
          Target = input("Choose target:")
          if Target == 1:
             print("You struck the first Orc")
-            HP = HP - (WP - AP)
-            return(HP,MP)
+            HP = HP - (WP - AP1)
          elif Target == 2:
             print("You struck the second Orc")
-            HP = HP - (WP - AP)
-            return(HP,MP)
+            HP = HP - (WP - AP2)
          elif Target == 3:
             print("You struck the third Orc")
-            HP = HP - (WP - AP)
-            return(HP,MP)
+            HP = HP - (WP - AP3)
+         elif Target == 4:
+            print("You struck the forth Orc")
+            HP4 = HP4 - (WP - AP4)
+            return(HP1,HP2,HP3,HP4,HP5,HP6,MP)
 
 #The enemy Orc turns
-def Orc1Turn(HP,WP,AP):
+def Orc1Turn(HP1,HP2,WP,AP1,AP2):
             print("The first Orc strikes!")
             print(random.random())
-            HP = HP - (WP -AP)
-            return(HP)  
+            if random.ramdom == 0:
+             HP1 = HP1 - (WP -AP1)
+            elif random.ramdom == 1:
+             HP2 = HP2 - (WP -AP2)
+            return(HP1,HP2)  
 
 
-def Orc2Turn(HP,WP,AP):
+def Orc2Turn(HP1,HP2,WP,AP1,AP2):
             print("The Second Orc strikes!")
             print(random.random())
-            HP = HP - (WP -AP)
-            return(HP) 
+            if random.ramdom == 0:
+             HP1 = HP1 - (WP -AP1)
+            elif random.ramdom == 1:
+             HP2 = HP2 - (WP -AP2)
+            return(HP1,HP2) 
 
     
-def Orc3Turn(HP,WP,AP):
+def Orc3Turn(HP1,HP2,WP,AP1,AP2):
             print("The third Orc strikes!")
             print(random.random())
-            HP = HP - (WP -AP)
-            return(HP)        
+            if random.ramdom == 0:
+             HP1 = HP1 - (WP -AP1)
+            elif random.ramdom == 1:
+             HP2 = HP2 - (WP -AP2)
+            return(HP1,HP2)
+
+def Orc4Turn(HP1,HP2,WP,AP1,AP2):
+            print("The forth Orc strikes!")
+            print(random.random())
+            if random.ramdom == 0:
+             HP1 = HP1 - (WP -AP1)
+            elif random.ramdom == 1:
+             HP2 = HP2 - (WP -AP2)
+            return(HP1,HP2)          
